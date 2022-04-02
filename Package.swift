b99197ea28d5d23ed8a812dce3adee23f98da646
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2022-04-02 22:28:31 +0200

    Add CI script for documentation generation

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
